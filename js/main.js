26da9874c522ff3ecbe403b119bb2c6b8a9e28e6
wallanpsantos <wallanpereira09@gmail.com>
2023-04-23 00:30:40 -0300

    Adicionado inserir 'li' via javascript

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 const form = document.getElementById('novoItem');
+const lista = document.getElementById('lista');
 
 
 function criaElemento(nome, quantidade) {
@@ -11,12 +12,17 @@ function criaElemento(nome, quantidade) {
     novoItem.appendChild(numeroItem);
     novoItem.innerHTML += nome;
 
-    const lista = document.getElementById('lista');
     lista.appendChild(novoItem);
+}
+
+function somaElemento(nome, quantidade) {
+    const lista = document.getElementById('lista');
+    console.log(lista.children[0].lastChild.data) //Output: Camisas
 }
 
 form.addEventListener('submit', (evento) => {
     evento.preventDefault();
 
     criaElemento(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value);
+    somaElemento(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value);
 })
